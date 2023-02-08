6bac6374479b831f41fe2b6b7e616ec0b8bd6397
machaoxue <machaoxue@yeah.net>
2023-02-08 15:21:59 +0800

    feat(solutions): 两数之和

---
+++ solutions-1-100/two-sum.ts
@@ -0,0 +1,44 @@
+// 两数之和 https://leetcode.cn/problems/two-sum/
+
+/**
+ * 简单版，变量两遍
+ * @param nums
+ * @param target
+ */
+function twoSum(nums: number[], target: number): number[] {
+  for (let i = 0; i <= nums.length - 2; i++) {
+    for (let j = i + 1; j <= nums.length - 1; j++) {
+      if (nums[j] === target - nums[i]) {
+        return [i, j];
+      }
+    }
+  }
+  return [-1, -1];
+}
+
+// const nums = [2, 7, 11, 15];
+// const target = 9;
+// console.log(twoSum(nums, target));
+
+/**
+ * 使用哈希表存储所遍历的数字与其下标，只需要遍历一遍
+ * @param nums
+ * @param target
+ */
+function twoSum2(nums: number[], target: number): number[] {
+  const map = new Map();
+  for (let i = 0; i <= nums.length - 1; i++) {
+    let s = target - nums[i];
+    console.log(s);
+    if (Number.isInteger(map.get(target - nums[i]))) {
+      return [i, map.get(target - nums[i])];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+  return [-1, -1];
+}
+
+// const nums = [2, 7, 11, 15];
+// const target = 9;
+// console.log(twoSum2(nums, target));
